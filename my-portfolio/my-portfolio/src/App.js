07be93890f6ab810da5e73990ac36841a3591a6f
Shruti Kapadia <59693294+shrutikapadia@users.noreply.github.com>
2024-04-11 13:38:57 -0400

    Add files via upload

---
+++ my-portfolio/my-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+   return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
